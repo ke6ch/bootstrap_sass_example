27ec73309849e4ec662a3be557c1b4d8077ee57a
ke6ch <19749261+ke6ch@users.noreply.github.com>
2020-07-25 23:36:34 +0900

    Add slick

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 import slick from 'slick-carousel';
+
+$('.slider--hero-image').slick({
+  autoplay:true,
+  autoplaySpeed:5000,
+  dots:true,
+  infinite: true,
+});
+
+$('.slider--multi-image').slick({
+  autoplay:true,
+  autoplaySpeed:5000,
+  dots:true,
+  infinite: true,
+  slidesToShow: 3,
+});
